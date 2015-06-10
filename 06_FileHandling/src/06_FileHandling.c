5360b6ac322335e3f4fa9146e1a3d0020de6a994
Lucas Engel <engellucas@gmx.de>
2015-06-10 07:33:10 +0200

    FileHandling - added metadata read - added return for main()

--- 06_FileHandling/src/06_FileHandling.c
+++ 06_FileHandling/src/06_FileHandling.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 int main()
 {
 	int file;
-	char buf[] = "Hallo Welt!"; // String (char-Array) which will be written into the file
+	char buf[] = "Hallo Welt! Hallo FileHandling!"; // String (char-Array) which will be written into the file
 	char filePath[] = "./06_FileHandling"; // relative-filepath
+	struct stat fileMetaData;
 
 	file = open(filePath, O_CREAT || O_RDWR);
 
@@ -15,4 +17,10 @@ int main()
 	write(file, buf, sizeof(buf) - 1);
 	read(file, buf, sizeof(buf) - 1);
 	close(file);
+
+
+	stat(filePath, &fileMetaData);
+	printf("Dateigröße: %d Bytes\n", fileMetaData.st_size);
+
+	return 1;
 }
